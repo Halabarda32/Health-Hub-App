0b3aafc302f1eb06d8acc421fc33bbd2dcc753b8
Halabarda32 <51050405+Halabarda32@users.noreply.github.com>
2024-01-03 19:31:21 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 export const firebaseConfig = {
-	apiKey: 'AIzaSyBjZ5BH8412krd1Fp3K0gKFhfhyJAvVAZw',
-	authDomain: 'react-auth-5b5a0.firebaseapp.com',
-	databaseURL: 'https://react-auth-5b5a0-default-rtdb.europe-west1.firebasedatabase.app',
-	projectId: 'react-auth-5b5a0',
-	storageBucket: 'react-auth-5b5a0.appspot.com',
-	messagingSenderId: '246256466123',
-	appId: '1:246256466123:web:16b84811f241a1de2d834e',
+	apiKey: '*****',
+	authDomain: '*****',
+	databaseURL: '*****',
+	projectId: '*****',
+	storageBucket: '*****',
+	messagingSenderId: '*****',
+	appId: '*****',
 }
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
